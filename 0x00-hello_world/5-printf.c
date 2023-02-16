5811d2da69aafca1ecfa1a30f6b0291dbef69c63
TselaneJulia <seepanetselane.com>
2023-02-16 11:40:44 -0800

    5 printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry point of a program
+ *
+ * Return: zero if no errors, non-zero if error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
